1f8b91d5aea58ccfd7dea515689ae360a3d49049
OussMz <163743313+OussMz@users.noreply.github.com>
2025-11-09 18:31:48 +0100

    .

---
+++ Week4/Ex3.py
@@ -0,0 +1,35 @@
+import string
+
+def pass_validator():
+    p = input("Enter your desired password: ")
+    if has_min_length(p) and has_uppercase(p) and has_lowercase(p) and has_digit(p) and has_special_char(p):
+        print("the entered password is valid")
+        return
+    print("the entered password is NOT valid!")
+    pass_validator()
+
+def has_min_length(p):
+    return len(p) >= 12
+
+def has_uppercase(p):
+    for char in p:
+        if char.isupper():
+            return True
+    return False
+def has_lowercase(p):
+    for char in p:
+        if char.islower():
+            return True
+    return False
+def has_digit(p):
+    for char in p:
+        if char.isdigit():
+            return True
+    return False
+def has_special_char(p):
+    for char in p:
+        if char in string.punctuation:
+            return True
+    return False
+
+pass_validator()
